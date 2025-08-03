d2d87f0e57361145b23d9c05449b20315a6d0132
saemii-24 <hims2419@gmail.com>
2025-08-03 21:18:36 +0900

    feat: home

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
 "use client";
 import ChatForm from "@/components/ChatForm";
+import ChatMessage from "@/components/ChatMessage";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [room, setRoom] = useState("");
+  const [joined, setJoined] = useState(false);
+  const [messages, setMessages] = useState<
+    {
+      sender: string;
+      message: string;
+    }[]
+  >([]);
+  const [userName, setUserName] = useState("");
   const handleSendMessage = (message: string) => {
     console.log(message);
   };
+
   return (
     <div className="flex mt-24 justify-center w-full">
       <div className="w-full max-w-3xl mx-auto">
         <h1 className="mb-4 text-2xl font-bold">Room: 1</h1>
-        <div>TODO: Add chat room</div>
+        <div>
+          {messages.map((msg, index) => {
+            <ChatMessage
+              key={index}
+              sender={msg.sender}
+              message={msg.message}
+              isOwnMessage={msg.sender === userName}
+            />;
+          })}
+        </div>
         <ChatForm onSendMessage={handleSendMessage} />
       </div>
     </div>
